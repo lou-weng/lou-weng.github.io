b27e8d869d623ab1b06128b69120dedb5147c779
watshever <louieweng@outlook.com>
2020-08-07 16:35:06 -0400

    final edit of the day

--- static_files/scripts/scripts.js
+++ static_files/scripts/scripts.js
@@ -1,4 +1,6 @@
-ScrollReveal().reveal('.home', { duration: 3000, interval:500 })
-ScrollReveal().reveal('.navigation', { duration: 3000, interval: 500 })
-ScrollReveal().reveal('.about', { duration: 3000 })
-ScrollReveal().reveal('.blog', { duration: 2000, interval: 250 })
+$(document).ready(function() {
+    ScrollReveal().reveal('.home', { duration: 3000, interval:500 })
+    ScrollReveal().reveal('.navigation', { duration: 3000, interval: 500 })
+    ScrollReveal().reveal('.about', { duration: 3000 })
+    ScrollReveal().reveal('.blog', { duration: 2000, interval: 250 })
+}
