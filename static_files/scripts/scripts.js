a8f206604e51a20c1eb0f42e1ac00a4932eab26b
watshever <louieweng@outlook.com>
2020-08-07 16:26:03 -0400

    added some animations

---
+++ static_files/scripts/scripts.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.home', { duration: 3000, interval:500 })
+ScrollReveal().reveal('.navigation', { duration: 3000, interval: 500 })
+ScrollReveal().reveal('.about', { duration: 3000 })
+ScrollReveal().reveal('.blog', { duration: 2000, interval: 250 })
