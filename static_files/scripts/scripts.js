2bd9f9742c0e39a85d30729d7a0bff07035d5761
watshever <louieweng@outlook.com>
2020-08-07 16:39:47 -0400

    fixed error in script

--- static_files/scripts/scripts.js
+++ static_files/scripts/scripts.js
@@ -3,4 +3,4 @@ $(document).ready(function() {
     ScrollReveal().reveal('.navigation', { duration: 3000, interval: 500 })
     ScrollReveal().reveal('.about', { duration: 3000 })
     ScrollReveal().reveal('.blog', { duration: 2000, interval: 250 })
-}
+})
