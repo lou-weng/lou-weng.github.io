8864e7a25c99f573ce99cd1836fc03f440e1d49c
watshever <louieweng@outlook.com>
2020-08-10 15:16:46 -0400

    image gallery added

--- static_files/scripts/scripts.js
+++ static_files/scripts/scripts.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
     ScrollReveal().reveal('.about', { duration: 3000, interval: 500 })
     ScrollReveal().reveal('.blog', { duration: 2000, interval: 250 })
     ScrollReveal().reveal('.journey', { duration: 3000, interval: 250 })
+    ScrollReveal().reveal('.interests', { duration: 3000, interval: 500 })
 })
 
 $(window).scroll(function () {
