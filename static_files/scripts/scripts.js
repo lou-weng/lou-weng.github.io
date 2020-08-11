05e7e4daeee311e05c63fb7e982c2cc539d3a5c6
watshever <louieweng@outlook.com>
2020-08-10 22:58:09 -0400

    finalized home page

--- static_files/scripts/scripts.js
+++ static_files/scripts/scripts.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-    ScrollReveal().reveal('.home', { duration: 2000, interval: 500 })
+    ScrollReveal().reveal('.home', { duration: 2000, interval: 300 })
     ScrollReveal().reveal('.navigation', { duration: 3000, interval: 500 })
     ScrollReveal().reveal('.about', { duration: 3000, interval: 500 })
     ScrollReveal().reveal('.blog', { duration: 2000, interval: 250 })
-    ScrollReveal().reveal('.projects', { duration: 2000, interval: 250 })
+    ScrollReveal().reveal('.projects', { duration: 3000, interval: 250 })
     ScrollReveal().reveal('.journey', { duration: 3000, interval: 250 })
     ScrollReveal().reveal('.interests', { duration: 3000, interval: 100 })
 
