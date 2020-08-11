5f4aeccca043ddde580a0a49eedc5680b15fdb4a
watshever <louieweng@outlook.com>
2020-08-10 22:20:51 -0400

    projects section added

--- static_files/scripts/scripts.js
+++ static_files/scripts/scripts.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     ScrollReveal().reveal('.navigation', { duration: 3000, interval: 500 })
     ScrollReveal().reveal('.about', { duration: 3000, interval: 500 })
     ScrollReveal().reveal('.blog', { duration: 2000, interval: 250 })
+    ScrollReveal().reveal('.projects', { duration: 2000, interval: 250 })
     ScrollReveal().reveal('.journey', { duration: 3000, interval: 250 })
     ScrollReveal().reveal('.interests', { duration: 3000, interval: 100 })
 
